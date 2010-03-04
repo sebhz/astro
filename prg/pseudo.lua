13f332028a02bd530b1c27002f84522d1395f244
sebhz <seb@zyglute.fr>
2010-03-04 16:46:59 +0100

    Added astrology tes in pseudo.lua

--- prg/pseudo.lua
+++ prg/pseudo.lua
@@ -3,5 +3,9 @@ require "astro.pseudoscience"
 
 local cal_to_jd = astro.calendar.cal_to_jd
 local biorhythm = astro.pseudoscience.biorhythm
+local astrology = astro.pseudoscience.astrology
 
 print(biorhythm(cal_to_jd(1973, 4, 28.75)))
+
+print(astrology("Venus", cal_to_jd(1973, 4, 28.75)))
+print(astrology("Venus"))
