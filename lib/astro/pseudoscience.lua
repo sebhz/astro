0da41d6b9ccc22b684139ccb36dafcdadeea6de6
sebhz <seb@zyglute.fr>
2010-03-04 16:35:28 +0100

    Added astrology to pseudosciences

--- lib/astro/pseudoscience.lua
+++ lib/astro/pseudoscience.lua
@@ -1,13 +1,22 @@
 require "astro.calendar"
 require "astro.constants"
 require "astro.util"
+require "astro.vsop87d"
+require "astro.nutation"
+require "astro.coordinates"
 
-local cal_to_jd       = astro.calendar.cal_to_jd
-local seconds_per_day = astro.constants.seconds_per_day
-local pi2             = astro.constants.pi2
-local round           = astro.util.round
+local cal_to_jd         = astro.calendar.cal_to_jd
+local seconds_per_day   = astro.constants.seconds_per_day
+local days_per_second   = astro.constants.days_per_second
+local pi2               = astro.constants.pi2
+local round             = astro.util.round
+local geocentric_planet = astro.vsop87d.geocentric_planet
+local nut_in_lon        = astro.nutation.nut_in_lon
+local true_obliquity    = astro.nutation.true_obliquity
+local equ_to_ecl        = astro.coordinates.equ_to_ecl
+
 --[[
-	Computes the 3 major biorhythms
+	Computes some  biorhythms
 --]]
 local function biorhythm(jd_origin, jd_current)
 	if jd_current == nil then 
@@ -25,5 +34,25 @@ local function biorhythm(jd_origin, jd_current)
 	return physical, emotional, intellectual, intuitive
 end
 
+local function astrology(body, jd)
+	local signs = { "Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius", "Capricorn", "Aquarius", "Pisces" };
+	
+	if jd == nil then 
+		local d = os.date("*t")
+		local fday = (d.hour*3600+d.min*60+d.sec)/seconds_per_day
+		jd = cal_to_jd(d.year, d.month, d.day+fday)
+	end
+	
+	local deltaPsi = nut_in_lon(jd)
+	local epsilon  = true_obliquity(jd)
+	local ra, decl = geocentric_planet(jd, body, deltaPsi, epsilon, days_per_second)
+	local long, lat = equ_to_ecl(ra, decl, epsilon)
+	
+	local i = 1+math.floor(math.deg(long)/30);
+	
+	return signs[i]
+end
+i
 if astro == nil then astro = {} end
-astro["pseudoscience"] = { biorhythm = biorhythm }
+astro["pseudoscience"] = { biorhythm = biorhythm,
+                           astrology = astrology }
